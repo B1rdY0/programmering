61b2d8e72e52e73979370f8deecef6603e272aeb
Aaryan hegde <hegde.aaryan@gmail.com>
2021-09-14 10:46:02 +0200

    Start

---
+++ MadLibs/MadLibs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+Console.WriteLine("HEYYYYYYYYYYYYYYY WHO ARE YOU?: ");
+
+string name; 
+
+name = Console.ReadLine();
+
+Console.WriteLine($"hey {name} that's pretty bruh");
+
+
+Console.WriteLine("let me ask you a question?");
+
+string name_2;
+
+name_2 = Console.ReadLine();
+
+Console.WriteLine("u like games?");
+
+
+
+
+string answer;
+answer = Console.ReadLine();
+
+
+if (answer == "yes" )
+{
+   Console.WriteLine("Nice what games u like"); 
+}
+
+if (answer == "nah")
+{
+    Console.WriteLine("eh ur boring, bye" );
+}
+
+
+if (answer == "somewhat")
+{ 
+    Console.WriteLine("let me show you some good games");
+    {
+        
+    }
+}
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
